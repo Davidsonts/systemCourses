7faad6235a404933c0f8f7ca66ef3f7cc103a174
Davidson <davidsonts@outlook.com>
2020-09-12 12:28:29 -0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create name: 'David', state: :active, kind: :manager, email: 'manager@gmail.com', password: 123456
+User.create name: 'Alan', state: :active, kind: :salesman, email: 'alan@gmail.com', password: 123456
