bd764aaf73f5f799a58317ebae5f30e39bd580ea
Davidson <davidsonts@outlook.com>
2020-09-12 12:06:58 -0300

    date to data

--- db/migrate/20200912144439_create_publications.rb
+++ db/migrate/20200912144439_create_publications.rb
@@ -2,7 +2,7 @@ class CreatePublications < ActiveRecord::Migration[6.0]
   def change
     create_table :publications do |t|
       t.string :title
-      t.data :date
+      t.date :data
       t.string :kind
       t.references :advisor, null: false, foreign_key: true
 
